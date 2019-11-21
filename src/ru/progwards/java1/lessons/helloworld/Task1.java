52f81c8af9d5f60ac923dbde70d350c88a915f7a
username <username@domaim.com>
2019-11-21 19:10:59 +0300

    H1 Домашнее задание: Задача 1. Класс Task1

---
+++ src/ru/progwards/java1/lessons/helloworld/Task1.java
@@ -0,0 +1,15 @@
+package ru.progwards.java1.lessons.helloworld;
+
+public class Task1 {
+    public static void main(String[] args) {
+        String msg1 = "Хорошо идут дела";
+        String msg2 = "Изучаю Java я!";
+        String msg3 = " ";
+
+        System.out.println(msg1);
+        System.out.println(msg2);
+        System.out.print(msg1 + msg3 + msg2);
+        System.out.println();
+        System.out.println(msg2 + msg3 + msg1);
+    }
+}
